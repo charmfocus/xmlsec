61c4605360a5ea415b0ce6d37a6b359587f260c5
pengkebao <84665381@qq.com>
2018-11-21 15:38:19 +0800

    first commit

---
+++ XMLSecEnc.go
@@ -0,0 +1,208 @@
+package xmlsec
+
+import (
+	"io"
+
+	"crypto/sha1"
+
+	"bufio"
+	"bytes"
+	"io/ioutil"
+
+	"encoding/base64"
+
+	"crypto"
+	"strings"
+
+	"errors"
+
+	"github.com/tinyhubs/tinydom"
+)
+
+const (
+	RSA_SHA256 = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"
+	XMLDSIGNS  = "http://www.w3.org/2000/09/xmldsig#"
+	SHA1       = "http://www.w3.org/2000/09/xmldsig#sha1"
+	SHA256     = "http://www.w3.org/2001/04/xmlenc#sha256"
+	C14N       = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+	EnvSign    = "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
+)
+
+type XmlSec struct {
+	XMLDocument tinydom.XMLDocument
+}
+
+func newXmlSec(reader io.Reader) (*XmlSec, error) {
+	xmlSec := new(XmlSec)
+	doc, err := tinydom.LoadDocument(reader)
+	if err != nil {
+		return xmlSec, err
+	}
+	xmlSec.XMLDocument = doc
+	return xmlSec, err
+}
+
+func Sign(data []byte, privateKey []byte) (value []byte, err error) {
+	return getSignElement(data, privateKey)
+}
+
+func getSignElement(data []byte, privateKey []byte) (value []byte, err error) {
+	signElement, err := newXmlSec(bytes.NewReader(data))
+	if err != nil {
+		return value, err
+	}
+	signElement.addSignatureElement(privateKey)
+	signElement.XMLDocument.InsertFirstChild(tinydom.NewProcInst("xml", `version="1.0" encoding="UTF-8"`))
+	value, err = elementToBytes(signElement.XMLDocument)
+	return
+}
+
+func (this *XmlSec) addSignatureElement(privateKey []byte) error {
+	documentElement := this.XMLDocument.FirstChildElement("document")
+	signatureElement := tinydom.NewElement("ds:Signature")
+	signatureElement.SetAttribute("xmlns:ds", XMLDSIGNS)
+	//需要签名的内容开始
+	signedInfoElement := tinydom.NewElement("ds:SignedInfo")
+
+	canonicalizationMethodElement := tinydom.NewElement("ds:CanonicalizationMethod")
+	canonicalizationMethodElement.SetAttribute("Algorithm", C14N)
+
+	signatureMethodElement := tinydom.NewElement("ds:SignatureMethod")
+	signatureMethodElement.SetAttribute("Algorithm", RSA_SHA256)
+
+	referenceElement := tinydom.NewElement("ds:Reference")
+	referenceElement.SetAttribute("URI", "")
+
+	transformsElement := tinydom.NewElement("ds:Transforms")
+
+	transformElement := tinydom.NewElement("ds:SignatureMethod")
+	transformElement.SetAttribute("Algorithm", EnvSign)
+
+	digestMethodElement := tinydom.NewElement("ds:DigestMethod")
+	digestMethodElement.SetAttribute("Algorithm", SHA1)
+
+	//<ds:DigestValue></ds:DigestValue>
+	//sha1 签名
+	documentValue, err := elementToBytes(documentElement)
+	if err != nil {
+		return err
+	}
+	digestValue := this.createSha1(documentValue)
+	digestValueElement := tinydom.NewElement("ds:DigestValue")
+	digestValueElement.SetText(digestValue)
+
+	digestMethodElement.InsertEndChild(digestValueElement)
+	transformElement.InsertEndChild(digestMethodElement)
+	transformsElement.InsertEndChild(transformElement)
+	referenceElement.InsertEndChild(transformsElement)
+	signatureMethodElement.InsertEndChild(referenceElement)
+	canonicalizationMethodElement.InsertEndChild(signatureMethodElement)
+	signedInfoElement.InsertEndChild(canonicalizationMethodElement)
+	signatureElement.InsertEndChild(signedInfoElement)
+
+	//需要签名的内容结束
+	//reaSha256 签名
+	signeValue, err := elementToBytes(signedInfoElement)
+	if err != nil {
+		return err
+	}
+	//signinfo签名
+	signeValueText, err := this.rsaSha256Encode(signeValue, privateKey)
+	if err != nil {
+		return err
+	}
+	signatureValueElement := tinydom.NewElement("ds:SignatureValue")
+	signatureValueElement.SetText(signeValueText)
+	signatureElement.InsertEndChild(signatureValueElement)
+	documentElement.InsertEndChild(signatureElement)
+	return nil
+}
+
+func elementToBytes(element tinydom.XMLDocument) (value []byte, err error) {
+	byteBuffer := bytes.NewBuffer(make([]byte, 0))
+	bufioWriter := bufio.NewWriter(byteBuffer)
+
+	tinydom.SaveDocument(element, bufioWriter, tinydom.PrintStream)
+	bufioWriter.Flush()
+	value, err = ioutil.ReadAll(byteBuffer)
+	return
+}
+
+func (this *XmlSec) rsaSha256Encode(src []byte, privateKey []byte) (string, error) {
+	sig, err := signPKCS1v15(src, privateKey, crypto.SHA256)
+	if err != nil {
+		return "", err
+	}
+	sign := base64.StdEncoding.EncodeToString(sig)
+	return sign, nil
+}
+
+func (this *XmlSec) createSha1(s []byte) string {
+	h := sha1.New()
+	h.Write(s)
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+func VerifySign(data []byte, publicKey []byte) (bool, error) {
+	signElement, err := newXmlSec(bytes.NewReader(data))
+	if err != nil {
+		return false, err
+	}
+	doc := signElement.XMLDocument
+	eml := doc.FirstChildElement("document").FirstChildElement("Signature").FirstChildElement("SignatureValue")
+	if eml == nil {
+		return false, errors.New("未取到sign")
+	}
+	signatureValue := eml.Text()
+	if len(signatureValue) < 1 {
+		return false, errors.New("未取到sign")
+	}
+
+	signValue := canonicalizeSignedInfo(data)
+	s, err := ioutil.ReadAll(signValue)
+	if err != nil {
+		return false, err
+	}
+	return rsaSha256Decode(s, signatureValue, publicKey)
+}
+
+func canonicalizeSignedInfo(data []byte) *bytes.Buffer {
+	byteBuffer := bytes.NewBuffer(data)
+	bufioReader := bufio.NewReader(byteBuffer)
+	start := false
+	for {
+		str, err := bufioReader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		if strings.Contains(str, "<SignedInfo>") {
+			str = strings.Replace(str, `>`, ` xmlns="`+XMLDSIGNS+`">`, -1)
+			start = true
+		}
+		if start {
+			if strings.Contains(str, "</SignedInfo>") {
+				line := []byte(str)
+				line = line[:len(line)-1]
+				if len(line) > 0 && line[len(line)-1] == '\r' {
+					line = line[:len(line)-1]
+				}
+				byteBuffer.WriteString(string(line))
+				break
+			}
+			byteBuffer.WriteString(str)
+		}
+	}
+	return byteBuffer
+}
+
+func rsaSha256Decode(data []byte, sign string, publicKey []byte) (bool, error) {
+	signBytes, err := base64.StdEncoding.DecodeString(sign)
+	if err != nil {
+		return false, err
+	}
+	err = verifyPKCS1v15(data, signBytes, publicKey, crypto.SHA256)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
---
+++ test.go
@@ -0,0 +1,57 @@
+package xmlsec
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var privateKey = []byte(`
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAzS4nzBr5qR+FfdIk6DgAkuER6RJ64sEL0exAGKVQv/XFvGgk
+f5nxNivzrm4ZSkeIwVTX9jxI6iefNDmVtMafPRTMcL8fGj4WIgtloreGjKHeMLJy
+eR38JihBGqUGrolBQpZBxu9e3GpUzYGUQ6i2q2bVFXjtbDz236aoy9sznZ1kwlsm
+RcNdVqGtTOvBnaSbrfgM0wObBdChGRgOLx+u80iowiIn27+Nr9+UU4SlVjh/osIl
+LbFpGuufsKBWZ7u7eIWcOsC3gCLhORPzvFmYrMBHImWnIeje/mgcdtfdjjQgSdqi
+YX6h/V/BG9XupMlJb0SpxHyBOaiYKsFAdBe4bwIDAQABAoIBAQCc4dOwLlgCxoVN
+ZSavIFLf+5O0QFsRkkW1ZwzDS3PossVUf9SYdF6I6yZRkwFnWDbqjDLspb+iulTy
+m60qIkyTaZnAA4KGkD5gHZLCzczlLsczON5qWVD45AKTmub3AR+TCEyXDWPc6Lia
+wZ9Z9GBnwiLfXi07e4nQDCA4bwyST+070fpyz6cJUPo7dEFU71Kh/r9+m7srZ1JL
+wsjiTnk5C8vyraRiDxg+jSyUKPLFYjmYd/XrkYvk11CbuktkM6TTsyDya4Aw38wf
+znCHlLCnbI6Cicd3qgr6AXtbeLaXHfaaS/gQuQ2jWmwh0q54u91c6qS/S7BJeEnG
+v6oCtUTxAoGBAPJGJOc2L8ndHl4abb84QSShcMRN0gdf8oKSDQQ8oNFpfO5/BxTK
+7VqpMnYdn+y6gogX7Qo/UywAcFCMJOQT+ZLcWna+S7fMu5TCN3rlw7Z7dmG4uL+u
+XzChm0QG5oS53rvHzNoSAPz7od/sqet+SXes6pN+Dssanz+MZzgf/NGFAoGBANjO
+BHR5WQUuNbBsynkoj27ouB2MdleAdXPWmfftnWhwDXuDj1rLUv1YU9JBwiaG0jQE
+KvNpgy2CcFQAL1naiu18DhrcvASIIeLOk2UEIQOsvaP8Al+nMJyYDzjDYPFD96X1
+y4Ud1pDYdm5eZKWCgaCb4gXLaEm88gNg8n6YLYpjAoGAEHgSNCFr2DYOxWjYgwIo
+83uB9eYwW83650NUXTBd4j5gfrnl2lT9E4ei5YKYB3iaPPQbafHqDnitKdyM+XAe
+LAyMnbvL29+v9KnLKbCKwFHeN0BeVrFcySqDKnmpm+YirPMlCsHkjU3xsA4fcuGv
+ztHTVFliWg4iOyipL5iFEB0CgYEA0yvHlxNjK/KYhOFSTbS+EI9IVi+sTQpoXI4Y
+pi6hdUo/N6ErDoqgqPdRnahDTXdgYx30H+cTyE4WzxrnBCQj0bFVo8CAugFG0D3Z
+vzjIV2X3yTP7s+OLK9vIZ8eWMym6m8HPnDc1+Rt+L35tlfCKBkAPZQl86OXK7JOP
+XtMm+HUCgYBl0heVYs5jayK27ryNfNsTu2M01lbyjU6xbXqrsI/ny667Zfm8N57r
+PS45GDXSj1T2fkrHoeqFeBaRZYxH0jAdCRNzZZangWe0/3gQpsZRDYsLs4OzjyYW
+SCjACGz1ol5DsCHkFnylV/HTHpVdXPLU5vwij1CjUzXii2OfcjZ/LA==
+-----END RSA PRIVATE KEY-----`)
+
+var publicKey = []byte(`
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDOb4B1dnwONcW0RoJMa0IOq3O6jiqnTGLUpxEw
+2xJg+c7wsb6DBy5CAoR0w2ZjZ/BjKxGIQ+DoDg3NsHJeyuEjNF0/Ro/R5xVpFC5z4cBVSC2/gddz
+4a1EoGDJewML/Iv0yIw7ylB86++h23nRd079c5S9RZXurBfnLW2Srhqk2QIDAQAB
+-----END PUBLIC KEY-----`)
+
+func TestSign() {
+	f, _ := os.Open("test.xml")
+	s, _ := ioutil.ReadAll(f)
+	value, err := Sign(s, privateKey)
+	fmt.Println(string(value), err)
+}
+
+func TestVerifySign() {
+	f, _ := os.Open("test.xml")
+	s, _ := ioutil.ReadAll(f)
+	ok, err := VerifySign(s, publicKey)
+	fmt.Println(ok, err)
+}
